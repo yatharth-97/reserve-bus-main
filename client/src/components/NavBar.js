0a1b4d8ca9316bed3e396474c3dbc535d5875275
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-11-18 17:47:51 +0530

    [WIP] made some pages and components

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
 export default function NavBar() {
-  return (
-    <div className='m-0 p-0 box-border'>
-      <div className='h-[50px] flex justify-center border-b-[3px] border-b-[rgb(179,150,150)] border-solid'>
-        <div className='font-sans w-full max-w-screen-lg text-[white] flex items-center justify-between'>
-          <div className='flex items-center gap-[50px]'>
-            <div className='logo'>
-              <i className='uil uil-bus-school bus-logo'></i>
-              <span>RESERVE</span>
-            </div>
+  return <div>NavBar</div>;
+  // <div className='m-0 p-0 box-border'>
+  //   <div className='h-[50px] flex justify-center border-b-[3px] border-b-[rgb(179,150,150)] border-solid'>
+  //     <div className='font-sans w-full max-w-screen-lg text-[white] flex items-center justify-between'>
+  //       <div className='flex items-center gap-[50px]'>
+  //         <div className='logo'>
+  //           <i className='uil uil-bus-school bus-logo'></i>
+  //           <span>RESERVE</span>
+  //         </div>
 
-            <a href='/' className='tck'>
-              Ticket
-            </a>
-            <a href='/' className='cont'>
-              Contact Us
-            </a>
-          </div>
+  //         <a href='/' className='tck'>
+  //           Ticket
+  //         </a>
+  //         <a href='/' className='cont'>
+  //           Contact Us
+  //         </a>
+  //       </div>
 
-          <div className='btns'>
-            <button className='nav-btn'>Login</button>
-            <button className='reg-btn'>Register</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  //       <div className='btns'>
+  //         <button className='nav-btn'>Login</button>
+  //         <button className='reg-btn'>Register</button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
