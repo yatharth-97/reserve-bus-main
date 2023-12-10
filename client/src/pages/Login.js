73570721b2d1bcb6f347ee205396c3f057197a50
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-12-10 22:48:16 +0530

    created login and registration page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div>Login</div>;
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className='h-screen d-flex justify-content-center align-items-center'>
+      <div className='w-400 card p-3'>
+        <h1 className='text-lg'>Reserve - Login</h1>
+        <hr />
+        <Form layout='vertical' onFinish={onFinish}>
+          <Form.Item label='Email' name='email'>
+            <Input type='text' />
+          </Form.Item>
+          <Form.Item label='Password' name='password'>
+            <Input type='password' />
+          </Form.Item>
+
+          <div className='d-flex justify-content-between align-items-center'>
+            <Link to='/register'>Click Here To Register</Link>
+            <button className='secondary-btn' type='submit'>
+              Login
+            </button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 function Register() {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className='h-screen d-flex justify-content-center align-items-center'>
       <div className='w-400 card p-3'>
         <h1 className='text-lg'>Reserve - Register</h1>
         <hr />
-        <Form layout='vertical'>
-          <Form.Item label='Name'>
+        <Form layout='vertical' onFinish={onFinish}>
+          <Form.Item label='Name' name='name'>
             <Input type='text' />
           </Form.Item>
-          <Form.Item label='Email'>
+          <Form.Item label='Email' name='email'>
             <Input type='text' />
           </Form.Item>
-          <Form.Item label='Password'>
-            <Input type='text' />
+          <Form.Item label='Password' name='password'>
+            <Input type='password' />
           </Form.Item>
+
+          <div className='d-flex justify-content-between align-items-center'>
+            <Link to='/login'>Click Here To Login</Link>
+            <button className='secondary-btn' type='submit'>
+              Register
+            </button>
+          </div>
         </Form>
       </div>
     </div>
