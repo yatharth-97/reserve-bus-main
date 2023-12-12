93515c4df3fb8a6269851997fc52ea4a548d9aaa
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-12-13 00:20:28 +0530

    [feat] implemented authorization

--- client/src/components/PublicRoute.js
+++ client/src/components/PublicRoute.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function PublicRoute({ children }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      navigate('/');
+    }
+  }, []);
+
   return <div>{children}</div>;
 }
 
