ee5557e0dd8062b6e78406773c9f1439e18e4679
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-12-12 23:23:27 +0530

    [wip] implementing authorization

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,38 @@ import './resources/global.css';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import PublicRoute from './components/PublicRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
