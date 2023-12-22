741ec523b74f75dd7aea57988d6158f24feaaf25
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-12-22 22:12:53 +0530

    implemented add bus form

--- client/src/pages/Admin/AdminBuses.js
+++ client/src/pages/Admin/AdminBuses.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PageTitle from '../../components/PageTitle';
+import BusForm from '../../components/BusForm';
 
 function AdminBuses() {
-  return <div>AdminBuses</div>;
+  const [showBusForm, setShowBusForm] = useState(false);
+
+  return (
+    <div>
+      <div className='d-flex justify-content-between'>
+        <PageTitle title='Buses' />
+        <button className='primary-btn' onClick={() => setShowBusForm(true)}>
+          Add Bus
+        </button>
+      </div>
+
+      {showBusForm && (
+        <BusForm showBusForm={showBusForm} setShowBusForm={setShowBusForm} />
+      )}
+    </div>
+  );
 }
 
 export default AdminBuses;
